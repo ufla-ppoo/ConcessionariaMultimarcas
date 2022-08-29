26f4d5622e762ecd92f092570768f3b70abb7bb9
Julio Cesar Alves <julio.caburu@gmail.com>
2022-08-29 10:33:42 -0300

    Remoção da classe Uno

--- src/fabricaautomoveis/carros/Uno.java
+++
@@ -1,8 +0,0 @@
-package fabricaautomoveis.carros;
-
-public class Uno extends Carro {
-    
-    public Uno(String cor) {
-        super("Uno", Marca.FIAT, cor, 20000, Categoria.POPULAR);
-    }   
-}
